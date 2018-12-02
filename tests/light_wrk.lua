fc999a018d83067c74f6ff4c8e5e93b478510e5d
yibit <yibitx@126.com>
2018-12-02 11:24:16 +0800

    Fixed config

--- tests/light_wrk.lua
+++ tests/light_wrk.lua
@@ -1,8 +1,3 @@
--- curl -H 'Content-Type:application/json' -v -k http://192.168.1.33:8090/ussd/send_message/ \
--- {\"timeStamp\":1467272137,\"content\":\":0123456789abcABC测试\",
--- \"called_msisdn\":\"13120333084\",\"dialing_msisdn\":\"01053778375\"}
-
-wrk.method = "POST"
+wrk.method = "GET"
 wrk.headers["Content-Type"] = "application/json"
-wrk.body =
-    '{"timeStamp":1467272137,"content":":0123456789abcABC测试","called_msisdn":"13120333084","dialing_msisdn":"01053778375"}'
+wrk.body = "{}"
